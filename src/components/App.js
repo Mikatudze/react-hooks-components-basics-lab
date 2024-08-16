02bed8cf89fe69de3d8663ab9d73b637bb11d300
KENT <kwilliam866@gmail.com>
2024-08-16 17:51:41 +0300

    correction

--- src/components/App.js
+++ src/components/App.js
@@ -16,22 +16,20 @@ function Home() {
   );
 }
 
-
 function About() {
-  return(
+  return (
     <div id="about">
       <h2>About</h2>
     </div>
-  )
+  );
 }
-
 
 function App() {
   return (
     <div>
       <NavBar />
-      <Home/>
-      <About/>
+      <Home />
+      <About />
     </div>
   );
 }
